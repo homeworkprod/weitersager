e596380ef363fb2be0302f5537d9ddec149562ed
Jochen Kupperschmidt <homework@nwsnet.de>
2015-08-15 23:25:29 +0200

    Extracted command line argument parsing into a separate module.

--- weitersager.py
+++ weitersager.py
@@ -45,7 +45,6 @@ exit.
 :Version: 0.1
 """
 
-import argparse
 from collections import namedtuple
 from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -55,13 +54,10 @@ from threading import Thread
 from time import sleep
 
 from blinker import signal
-from irc.bot import ServerSpec, SingleServerIRCBot
+from irc.bot import SingleServerIRCBot
 from irc.buffer import LenientDecodingLineBuffer
 
-
-DEFAULT_HTTP_IP_ADDRESS = '0.0.0.0'
-DEFAULT_HTTP_PORT = 8080
-DEFAULT_IRC_PORT = ServerSpec('').port
+from weitersager.argparser import parse_args
 
 
 # A note on threads (implementation detail):
@@ -327,60 +323,6 @@ class Processor(object):
             sleep(0.5)
 
         log('Shutting down ...')
-
-
-# -------------------------------------------------------------------- #
-# command line argument parsing
-
-
-def parse_args():
-    """Setup and apply the command line arguments parser."""
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--irc-nickname',
-        dest='irc_nickname',
-        default='Weitersager',
-        help='the IRC nickname the bot should use',
-        metavar='NICKNAME')
-
-    parser.add_argument('--irc-realname',
-        dest='irc_realname',
-        default='Weitersager',
-        help='the IRC realname the bot should use',
-        metavar='REALNAME')
-
-    parser.add_argument('--irc-server',
-        dest='irc_server',
-        type=parse_irc_server_arg,
-        help='IRC server (host and, optionally, port) to connect to'
-            + ' [e.g. "irc.example.com" or "irc.example.com:6669";'
-            + ' default port: {:d}]'.format(DEFAULT_IRC_PORT),
-        metavar='SERVER')
-
-    parser.add_argument('--http-listen-ip-address',
-        dest='http_ip_address',
-        default=DEFAULT_HTTP_IP_ADDRESS,
-        help='the IP address to listen on for HTTP requests [default: {}]'
-             .format(DEFAULT_HTTP_IP_ADDRESS),
-        metavar='IP_ADDRESS')
-
-    parser.add_argument('--http-listen-port',
-        dest='http_port',
-        type=int,
-        default=DEFAULT_HTTP_PORT,
-        help='the port to listen on for HTTP requests [default: {:d}]'
-             .format(DEFAULT_HTTP_PORT),
-        metavar='PORT')
-
-    return parser.parse_args()
-
-
-def parse_irc_server_arg(value):
-    """Parse a hostname with optional port."""
-    fragments = value.split(':', 1)
-    if len(fragments) > 1:
-        fragments[1] = int(fragments[1])
-    return ServerSpec(*fragments)
 
 
 # -------------------------------------------------------------------- #
---
+++ weitersager/argparser.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+"""
+weitersager.argparser
+~~~~~~~~~~~~~~~~~~~~~
+
+Command line argument parsing
+
+:Copyright: 2007-2015 Jochen Kupperschmidt
+:License: MIT, see LICENSE for details.
+"""
+
+from argparse import ArgumentParser
+
+from irc.bot import ServerSpec
+
+
+DEFAULT_HTTP_IP_ADDRESS = '0.0.0.0'
+DEFAULT_HTTP_PORT = 8080
+DEFAULT_IRC_PORT = ServerSpec('').port
+
+
+def parse_args():
+    """Setup and apply the command line arguments parser."""
+    parser = ArgumentParser()
+
+    parser.add_argument('--irc-nickname',
+        dest='irc_nickname',
+        default='Weitersager',
+        help='the IRC nickname the bot should use',
+        metavar='NICKNAME')
+
+    parser.add_argument('--irc-realname',
+        dest='irc_realname',
+        default='Weitersager',
+        help='the IRC realname the bot should use',
+        metavar='REALNAME')
+
+    parser.add_argument('--irc-server',
+        dest='irc_server',
+        type=parse_irc_server_arg,
+        help='IRC server (host and, optionally, port) to connect to'
+            + ' [e.g. "irc.example.com" or "irc.example.com:6669";'
+            + ' default port: {:d}]'.format(DEFAULT_IRC_PORT),
+        metavar='SERVER')
+
+    parser.add_argument('--http-listen-ip-address',
+        dest='http_ip_address',
+        default=DEFAULT_HTTP_IP_ADDRESS,
+        help='the IP address to listen on for HTTP requests [default: {}]'
+             .format(DEFAULT_HTTP_IP_ADDRESS),
+        metavar='IP_ADDRESS')
+
+    parser.add_argument('--http-listen-port',
+        dest='http_port',
+        type=int,
+        default=DEFAULT_HTTP_PORT,
+        help='the port to listen on for HTTP requests [default: {:d}]'
+             .format(DEFAULT_HTTP_PORT),
+        metavar='PORT')
+
+    return parser.parse_args()
+
+
+def parse_irc_server_arg(value):
+    """Parse a hostname with optional port."""
+    fragments = value.split(':', 1)
+    if len(fragments) > 1:
+        fragments[1] = int(fragments[1])
+    return ServerSpec(*fragments)
