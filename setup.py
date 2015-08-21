e094def7ae5f7b59ef630c8952235782795e7803
Jochen Kupperschmidt <homework@nwsnet.de>
2015-08-21 04:48:59 +0200

    Include README as long description.

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import codecs
+
 from setuptools import setup
+
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
     name='Weitersager',
     version='0.1',
     description='A proxy to forward messages received via HTTP to to IRC',
+    long_description=long_description,
     author='Jochen Kupperschmidt',
     author_email='homework@nwsnet.de',
     url='http://homework.nwsnet.de/',
