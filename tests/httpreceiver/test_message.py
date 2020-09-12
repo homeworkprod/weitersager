50d76eacda1209774c88a9340137abe8147a5554
Jochen Kupperschmidt <homework@nwsnet.de>
2020-09-12 16:16:36 +0200

    Port tests from nose2 to pytest

--- tests/httpreceiver/test_message.py
+++ tests/httpreceiver/test_message.py
@@ -1,25 +1,25 @@
 import json
-from unittest import TestCase
 
-from nose2.tools import params
+import pytest
 
 from weitersager.httpreceiver import Message
 
 
-class MessageTestCase(TestCase):
-
-    @params(
+@pytest.mark.parametrize(
+    'channels, text',
+    [
         (['#example'             ], 'ohai, kthxbye!'                  ),
         (['#partyline', '#idlers'], 'Nothing to see here, move along.'),
-    )
-    def test_from_json(self, channels, text):
-        data = {
-            'channels': channels,
-            'text': text,
-        }
-        json_data = json.dumps(data)
+    ],
+)
+def test_from_json(channels, text):
+    data = {
+        'channels': channels,
+        'text': text,
+    }
+    json_data = json.dumps(data)
 
-        message = Message.from_json(json_data)
+    message = Message.from_json(json_data)
 
-        self.assertEqual(message.channels, frozenset(channels))
-        self.assertEqual(message.text, text)
+    assert message.channels == frozenset(channels)
+    assert message.text == text
--- tests/processor/test_message_handled.py
+++ tests/processor/test_message_handled.py
@@ -1,54 +1,47 @@
-from unittest import TestCase
-
 from weitersager.processor import Processor
 from weitersager.signals import channel_joined, message_approved, \
     message_received
 
 
-class MessageHandledTestCase(TestCase):
+def test_message_handled():
+    channel_names = ['#foo', '#bar']
+    text = 'Knock, knock.'
 
-    def setUp(self):
-        self.received_signal_data = []
+    received_signal_data = []
 
-    def test_message_handled(self):
-        channel_names = ['#foo', '#bar']
-        text = 'Knock, knock.'
+    @message_approved.connect
+    def handle_message_approved(sender, **data):
+        received_signal_data.append(data)
 
-        @message_approved.connect
-        def handle_message_approved(sender, **data):
-            self.storeReceivedSignalData(data)
+    processor = Processor()
+    processor.connect_to_signals()
 
-        processor = Processor()
-        processor.connect_to_signals()
+    fake_channel_joins(channel_names)
 
-        self.fake_channel_joins(channel_names)
+    send_message_received_signal(channel_names, text)
 
-        self.send_message_received_signal(channel_names, text)
+    assert received_signal_data == [
+        {
+            'channel_name': '#foo',
+            'text': text,
+        },
+        {
+            'channel_name': '#bar',
+            'text': text,
+        },
+    ]
 
-        self.assertReceivedSignalDataEqual([
-            {
-                'channel_name': '#foo',
-                'text': text,
-            },
-            {
-                'channel_name': '#bar',
-                'text': text,
-            },
-        ])
 
-    def fake_channel_joins(self, channel_names):
-        for channel_name in channel_names:
-            channel_joined.send(channel_name=channel_name)
+def fake_channel_joins(channel_names):
+    for channel_name in channel_names:
+        channel_joined.send(channel_name=channel_name)
 
-    def send_message_received_signal(self, channel_names, text):
-        source_address = ('127.0.0.1', 12345)
-        message_received.send(None,
-                              channel_names=channel_names,
-                              text=text,
-                              source_address=source_address)
 
-    def storeReceivedSignalData(self, data):
-        self.received_signal_data.append(data)
-
-    def assertReceivedSignalDataEqual(self, expected):
-        self.assertEqual(self.received_signal_data, expected)
+def send_message_received_signal(channel_names, text):
+    source_address = ('127.0.0.1', 12345)
+    message_received.send(
+        None,
+        channel_names=channel_names,
+        text=text,
+        source_address=source_address,
+    )
