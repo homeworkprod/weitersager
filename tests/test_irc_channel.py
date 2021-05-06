8d5374f5d9b29069783d3682ecc1d79e8eb07a07
Jochen Kupperschmidt <homework@nwsnet.de>
2021-05-06 23:02:23 +0200

    Test IRC bot, channel

---
+++ tests/test_irc_channel.py
@@ -0,0 +1,21 @@
+"""
+:Copyright: 2007-2021 Jochen Kupperschmidt
+:License: MIT, see LICENSE for details.
+"""
+
+import pytest
+
+from weitersager.irc import IrcChannel
+
+
+@pytest.mark.parametrize(
+    'channel, expected_name, expected_password',
+    [
+        (IrcChannel('#example'),                         '#example',      None    ),
+        (IrcChannel('#example', password=None),          '#example',      None    ),
+        (IrcChannel('#headquarters', password='secret'), '#headquarters', 'secret'),
+    ],
+)
+def test_irc_channel_creation(channel, expected_name, expected_password):
+    assert channel.name == expected_name
+    assert channel.password == expected_password
